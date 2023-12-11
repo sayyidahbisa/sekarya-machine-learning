7867c7e4f54d4e6de5a0b862f78017763b426724
Moh Prayogi <56709331+Magnuver@users.noreply.github.com>
2023-12-11 20:51:27 +0800

    nambah

--- Deploy-Test/main.py
+++ Deploy-Test/main.py
@@ -1,17 +1,38 @@
 from flask import Flask, request, jsonify
 from waitress import serve
-from predict import predict_class
+import os
+import numpy as np
+import tensorflow as tf
+from keras.utils import load_img, img_to_array
 import json
+
+# Load the ML model
+model = tf.keras.models.load_model('./model.h5')
+
+# Image labels
+labels = {0: 'AI_GENERATED',
+          1: "NON_AI_GENERATED"}
+
+# Used to preprocess images into array before predicting them
+def preprocess_image(image_path):
+    image = load_img(image_path, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    return image
+
+# Used to predict the images' class
+def predict_image(image):
+    prediction = model.predict(image, verbose=0)
+    return labels.get(prediction[0, 0])
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1000 * 1000
-
 
 @app.route("/predict", methods=['POST'])
 def index():
     if request.method == 'POST':
         # Mengambil Kunci API dari Request Header
-        key = request.headers.get('X-API-Key')
+        key = request.headers.get('api-key')
         # Jika Kunci API Benar
         if (key == api_key):
             # Try (jika request valid)
@@ -19,7 +40,8 @@ def index():
                 image_file = request.files['image']
                 image_file.save('uploaded_image.jpg')
 
-                prediksi_label = predict_class('uploaded_image.jpg')
+                image = preprocess_image('uploaded_image.jpg')
+                prediksi_label = predict_image(image)
 
                 result = {'predicted_class': prediksi_label}
                 return jsonify(result)
